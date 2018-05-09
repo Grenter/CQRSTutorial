da3bc36403b5c3a3cfb4b15a8688046848e594a8
David Grenter <davidgrenter@gmail.com>
2018-05-09 12:12:20 +0100

    Create CommandService and CommandService.Tests project

---
+++ CQRSTutorial.Cafe.CommandService/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceProcess;
+
+namespace CQRSTutorial.Cafe.CommandService
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var servicesToRun = new ServiceBase[]
+            {
+                new TabCommandService() 
+            };
+            ServiceBase.Run(servicesToRun);
+        }
+    }
+}
---
+++ CQRSTutorial.Cafe.CommandService/TabCommandService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceProcess;
+
+namespace CQRSTutorial.Cafe.CommandService
+{
+    public class TabCommandService : ServiceBase
+    {
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
