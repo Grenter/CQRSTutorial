515b1626f4835b70be726ded5ea860118da27d9d
David Grenter <davidgrenter@gmail.com>
2018-06-27 16:36:32 +0100

    Add simple InMemoryMessageBus

---
+++ CQRSTutorial.Messaging.Tests/InMemoryMessageBusTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using CQRSTutorial.Core;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace CQRSTutorial.Messaging.Tests
+{
+    [TestFixture]
+    public class InMemoryMessageBusTests
+    {
+        [Test]
+        public void Registered_handlers_recieve_raised_events()
+        {
+            var bus = new InMemoryMessageBus();
+            var eventHandler = Substitute.For<IEventHandler>();
+            var fakeEvent = new FakeEvent();
+
+            bus.RegisterEventHandler(eventHandler);
+
+            bus.RaiseEvent<FakeEvent>(fakeEvent);
+
+            eventHandler.Received().Handle(fakeEvent);
+        }
+    }
+
+    public class FakeEvent : IDomainEvent
+    {
+        public Guid Id { get; set; }
+        public Guid AggregateId { get; set; }
+    }
+
+    public class EventHandler : IEventHandler
+    {
+        public void Handle(IDomainEvent domainEvents)
+        {
+            
+        }
+    }
+}
---
+++ CQRSTutorial.Messaging/InMemoryMessageBus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using CQRSTutorial.Core;
+
+namespace CQRSTutorial.Messaging
+{
+    public class InMemoryMessageBus : IMessageBus
+    {
+        IList<IEventHandler> _handlers = new List<IEventHandler>();
+
+        public void RegisterEventHandler<T>(T eventHandler) where T : IEventHandler
+        {
+            _handlers.Add(eventHandler);
+        }
+
+        public void RaiseEvent<T>(IDomainEvent domainEvent) where T : IDomainEvent
+        {
+            var @event = (T)domainEvent;
+
+            foreach (var eventHandler in _handlers)
+            {
+                eventHandler.Handle(@event);
+            }
+        }
+    }
+
+    public interface IMessageBus
+    {
+        void RegisterEventHandler<T>(T eventHandler) where T : IEventHandler;
+
+        void RaiseEvent<T>(IDomainEvent domainEvent) where T : IDomainEvent;
+    }
+}
